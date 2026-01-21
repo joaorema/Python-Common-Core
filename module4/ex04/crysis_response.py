78babc25e30c6af7360d1fb05ccc1d9d8723a4ee
joaorema <joaopcrema@gmail.com>
2026-01-21 13:34:26 +0000

    Final exercise complete starting now module5

--- module4/ex04/crysis_response.py
+++ module4/ex04/crysis_response.py
@@ -4,9 +4,41 @@ import sys
 
 def crisis_response():
     print(f"=== Cyber ARCHIVES - CRISIS RESPONSE SYSTEM ===")
-    pass
+    print()
+    try:
+        print(f"CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
+        with open("lost_archive.txt", "r") as file:
+            print(file.read())
+            print(f"STATUS: Normal operation resumed")
+    except FileNotFoundError:
+        print(f"RESPONSE: Archive not found in storage matrix")
+        print(f"STATUS: Crisis handled, security maintained")
+    finally:
+        print()
 
+    try:
+        print(f"CRISIS ALERT: Attempting access to 'classified_data.txt'...")
+        with open("classified_data.txt", "r") as file:
+            print(file.read())
+    except PermissionError as error:
+        print(f"[RESPONSE]: Security protocol deny access")
+        print(f"STATUS: Crisis handled, security maintained")
+    finally:
+        print()
+
+    try:
+        print(f"ROUTINE ACCESS: Attempting access to 'standard_archive.txt'...")
+        with open("standard_archive.txt", "r") as file:
+            print(f"[SUCCESS]: Archive recovered - {file.read()}")
+            print(f"STATUS: Normal operation resumed")
+    except (PermissionError,FileExistsError, FileNotFoundError) as error:
+        print(f"[RESPONSE]: {error}")
+        print(f"STATUS: Crisis handled, security maintained")
+    finally:
+        print()
+
+    print(f"All crisis scenarios handled successfully. Archive secure.")
 
 if __name__ == "__main__":
-    pass
+    crisis_response()
 
